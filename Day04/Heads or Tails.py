a9eeadc7699faf91b160bf9a6878a045c61ede83
Aruana <aruanagarcia@gmail.com>
2023-08-07 13:40:52 -0300

    Exercício cara e coroa.

---
+++ Day04/Heads or Tails.py
@@ -0,0 +1,30 @@
+# Instructions
+# You are going to write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails".
+#
+# Important, the first letter should be capitalised and spelt exactly like in the example
+# e.g. Heads, not heads.
+#
+# There are many ways of doing this. But to practice what we learnt in the last lesson,
+# you should generate a random number, either 0 or 1. Then use that number to print out
+# Heads or Tails.
+#
+# e.g. 1 means Heads 0 means Tails
+#
+# Example Output
+#
+# Heads
+# or
+#
+# Tails
+# When you're happy with your code, click submit.
+
+import random
+
+random_integer = random.randint(0, 1)
+
+
+if random_integer == 0:
+ print('Heads')
+
+else:
+  print('Tails')
