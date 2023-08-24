1b92dde349d6f2f9668c1144e927d02bcfbd18d3
Aruana <aruanagarcia@gmail.com>
2023-08-24 01:34:27 -0300

    Pep8 corrections.

--- Day07/Day-7-Hangman-1-start.py
+++ Day07/Day-7-Hangman-1-start.py
@@ -1,23 +1,21 @@
 import random
-#Step 1 
+# Step 1
 
 word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
 guess = input('Guess a letter: ').lower()
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
 for letter in chosen_word:
     if letter == guess:
         print('Rhight')
     else:
         print('Wrong')
-
-
